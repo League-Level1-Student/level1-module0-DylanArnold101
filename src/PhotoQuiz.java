dd02de5670ff083b88dc74ef8ba3f22c1963029d
DylanArnold101 <league@WTS7.attlocal.net>
2018-06-24 11:23:40 -0700

    gv
    hg

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,11 +22,11 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+                String jeffy = "cymbal.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+                Component dy ;
 		// 3. use the "createImage()" method below to initialize your Component
-
+                 	dy = createImage(jeffy);
 		// 4. add the image to the quiz window
 
 		// 5. call the pack() method on the quiz window
