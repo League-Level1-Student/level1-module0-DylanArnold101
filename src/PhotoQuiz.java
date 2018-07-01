e058a9961951f157b8050d63f6fc49d92630852c
DylanArnold101 <league@WTS7.attlocal.net>
2018-07-01 11:28:45 -0700

    Dylan
    jpg and urls

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -3,7 +3,6 @@
  *    Level 1
  */
 
-
 import java.awt.Component;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -19,36 +18,50 @@ public class PhotoQuiz {
 	public static void main(String[] args) throws Exception {
 		JFrame quizWindow = new JFrame();
 		quizWindow.setVisible(true);
-                quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
+		quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // This will make sure the program exits when you
+																	// close the window
 
-		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-                String jeffy = "cymbal.jpg";
+		// 1. find an image on the internet, and put its URL in a String variable (from
+		// your browser, right click on the image, and select “Copy Image Address”)
+		String jeffy = "cymbal.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-                Component dy ;
+		Component dy;
 		// 3. use the "createImage()" method below to initialize your Component
-                 	dy = createImage(jeffy);
+		dy = createImage(jeffy);
 		// 4. add the image to the quiz windoww
-
+		quizWindow.add(dy);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String r = JOptionPane
+				.showInputDialog("Which of these terrorists manufactures these drums? Bin laden,Obama,or alibaba");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if (r.equalsIgnoreCase("Obama")) {
+			JOptionPane.showMessageDialog(null, "you are correct");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
-
-		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+		else {
+			JOptionPane.showMessageDialog(null, "get played boy");
+		}
+		// 9. remove the component from the quiz window (you may not see the effect of
+		// this until step 12)
+		quizWindow.remove(dy);
 		// 10. find another image and create it (might take more than one line of code)
-
+		String pup = "puupy.jpg";
 		// 11. add the second image to the quiz window
-
+		Component puppy;
+		puppy = createImage(pup);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
-
+		String s = JOptionPane.showInputDialog("What breed is this dog?");
+		if (s.equalsIgnoreCase("Weimaraner")) {
+			JOptionPane.showMessageDialog(null, "you're correct");
+		}
 		// 14+ check answer, say if correct or incorrect, etc.
-
+		else {
+			JOptionPane.showMessageDialog(null, "get played boy!");
+		}
 	}
 
 	private static Component createImage(String imageUrl) throws MalformedURLException {
@@ -60,10 +73,6 @@ public class PhotoQuiz {
 
 	/* OPTIONAL */
 	// *14. add scoring to your quiz
-	// *15. make something happen when mouse enters image (imageComponent.addMouseMotionListener()) 
+	// *15. make something happen when mouse enters image
+	// (imageComponent.addMouseMotionListener())
 }
-
-
-
-
-
